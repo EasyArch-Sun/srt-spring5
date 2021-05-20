b63b503f80b1e31fef6af319407a46f8b1caefdb
EasyArch-Sun <3268543958@qq.com>
2021-05-20 23:12:03 +0800

    inside_bean

---
+++ src/com/srt/demo_ioc/di_other/Book.java
@@ -0,0 +1,28 @@
+package com.srt.demo_ioc.di_other;
+
+public class Book {
+
+
+    private String bname;
+    private String bauthor;
+    private String address;
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+
+
+    public void setBauthor(String bauthor) {
+        this.bauthor = bauthor;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void testDemo(){
+        System.out.println(bname+"::"+bauthor+"``"+address);
+    }
+
+
+}
