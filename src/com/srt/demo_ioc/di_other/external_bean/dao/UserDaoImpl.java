f5708c3196120fa2979953fa44c0ec2e4e8fbb1a
EasyArch-Sun <3268543958@qq.com>
2021-05-31 22:55:14 +0800

    external_bean

---
+++ src/com/srt/demo_ioc/di_other/external_bean/dao/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.srt.demo_ioc.di_other.external_bean.dao;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void update() {
+        System.out.println("dao update ..");
+
+    }
+}
